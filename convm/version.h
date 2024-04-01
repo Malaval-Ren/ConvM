a337673038154a1a61b4aeac70d45e4dae8255ec
Renaud Malaval <renaud.malaval@free.fr>
2024-04-01 13:50:19 +0200

    Finally, the version resource works

--- convm/version.h
+++ convm/version.h
@@ -1,5 +1,12 @@
-#define FILEVER_VSINFO      1,12,10,129  
-#define FILEVER_TEXT        "1.12.10.129"  
+//{{NO_DEPENDENCIES}}
+// fichier Include Microsoft Visual C++.
+// Utilisé par convm.rc
+//
+#define FILEVER_VSINFO      1,13,13,139  
+#define FILEVER_TEXT        "1.13.13.139"  
 #define COMPANY_NAME        "© Disk Crack Band ;)"  
 #define FILE_DESCRIPTION    "Converter to help in cross dev for Apple IIGS"  
+#define LEGAL_CORYRIGHT     "Copyright (©) 2022..2024 R. Malaval & F. Mure"
 #define PRODUCT_NAME        "ConvM"  
+#define PRODVER_VSINFO      1,13,13,139
+#define PRODVER_TEXT        "1.13.13.139"
